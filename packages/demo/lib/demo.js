b6492671a3024e8fe6ff6b7715ce5d0d888f98f0
LiZheGuang <1030660726@qq.com>
2020-06-02 15:41:17 +0800

    test

--- packages/demo/lib/demo.js
+++ packages/demo/lib/demo.js
@@ -2,7 +2,7 @@
 
 module.exports = demo;
 
-var boxs = "djdjjdjekldjedddddededeedd111ddtest";
+var boxs = "djdjjdjekldjedddddededeedd111ddt111est";
 function demo() {
   // TODO
   return "我是包管理工具662" + boxs;
